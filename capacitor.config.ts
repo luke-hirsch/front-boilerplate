b6e20e99a29057a083dccfab70b6fc44612852f2
Lukas von Hirschhausen <lukas@Lukass-MacBook-Pro.local>
2024-09-04 11:02:37 +0200

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.resi_app.de',
+  appName: 'resi-app',
+  webDir: 'dist'
+};
+
+export default config;
